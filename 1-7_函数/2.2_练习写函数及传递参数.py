d387bf65f433fcf296dbbaa0ec02fb98fcbfeeac
Yang Hu <hyytez@gmail.com>
2018-09-12 20:18:55 +0800

    update

---
+++ 1-7_函数/2.2_练习写函数及传递参数.py
@@ -0,0 +1,2 @@
+# TODO: 定义一个函数，名称为 creed
+# TODO: 在函数中，分行打印三个单词: "I", "Love", "Coding"
--- 1-7_函数/test.py
+++ 1-7_函数/test.py
@@ -1,4 +1,8 @@
-def greeting(*args):  # 这里的 * 号是通配符，后面的args意为参数。你也可以用别的单词，但是 * 必须保留
-    name, day, weather = args  # 用多个变量来将可传递参数进行解包，简单来说就是用一句话同时设置了两个变量
-    print("你好啊，{}！今天是{}！今天天气: {}".format(name, day, weather))
-greeting("hyy", "星期二", "晴")
+def poem():
+    print("窗前明月光")
+    print("疑似地上霜")
+    print("举头望明月")
+    print("低头思故乡")
+
+def author(project, author):
+    print("{}的作者是{}".format(project, author))
