606cc505c0a9f2badcca0346871b229c35f98a3c
Yang Hu <hyytez@gmail.com>
2018-09-13 16:01:50 +0800

    added another solution to ex3.py of course 1-6

--- 1-6_用逻辑创造程序分支/teacher/ex3.py
+++ 1-6_用逻辑创造程序分支/teacher/ex3.py
@@ -1,12 +1,27 @@
+# score = 59
+#
+# if score >= 60 and score < 70:
+#     print("你的成绩是：及格")
+# elif score >= 70 and score < 80:
+#     print("你的成绩是：还行")
+# elif score >= 80 and score < 90:
+#     print("你的成绩是：良好")
+# elif score >= 90 and score < 100:
+#     print("你的成绩是：陈独秀")
+# elif score == 100:
+#     print("你的成绩是：天秀")
+# else:
+#     print("你怕是不及格。")
+
 score = 59
 
-if score >= 60 and score < 70:
+if 60 <= score <= 69:
     print("你的成绩是：及格")
-elif score >= 70 and score < 80:
+elif 70 <= score <= 79:
     print("你的成绩是：还行")
-elif score >= 80 and score < 90:
+elif 80 <= score <= 89:
     print("你的成绩是：良好")
-elif score >= 90 and score < 100:
+elif 90 <= score <= 99:
     print("你的成绩是：陈独秀")
 elif score == 100:
     print("你的成绩是：天秀")
