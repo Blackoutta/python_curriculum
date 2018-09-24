e5835faa686c9f312a30fd729564b21c4d52a7f1
Yang Hu <hyytez@gmail.com>
2018-09-24 16:44:13 +0800

    update course 2-2 (completed)

--- 2-2_列表进阶/2.1-小心列表的可变性.py
+++ 2-2_列表进阶/2.1-小心列表的可变性.py
@@ -2,11 +2,34 @@
 但是同时，这也会引发一个问题：在迭代列表时，我们可能会遇到一些意想不到的情况。
 
 例子：
-    # 我们写一个函数，它可以在列表中随机推荐一款游戏
-    import random
-    video_games = ['漫威蜘蛛侠', '刺客信条:奥德赛', '黄金之国伊拉']
+    video_games = ['刺客信条：奥德赛', '黄金之国伊拉', '漫威蜘蛛侠', '荒野大镖客2']
 
-    def recommendation():
+    # 我们写一个函数，它会在一个列表中推荐一个随机游戏，然后打印出剩下的游戏
+    def recommendation(list):
         list_len = len(video_games)
-        random_number = random.randint(0, list_len)
-        print("今日推荐游戏为：{}".format(video_games.pop(random_number)))
+        random_number = random.randint(0, list_len - 1)
+        print("\n====> 今日推荐游戏为：{} <====".format(list.pop(random_number)))
+        print("\n近期热点：")
+        for game in list:
+            print(" * " + game)
+
+    recommendation(video_games)
+
+# 用 python -i 运行这个脚本，然后反复调用 recommendation(video_games)，看看会发生什么?
+
+--------------------------------------------------------------------------------
+# 没错，随着每次pop()，列表中的值会越来越少。在多次调用该函数后，列表被清空，从而导致程序错误。
+# 解决方法很简单，那就是用 .copy()这个方法
+
+    def recommendation(list):
+        list_copy = list.copy()  # 这里我们将传递进来的列表用 .copy()复制出一份镜像，并存储在新的变量中
+        list_len = len(video_games)
+        random_number = random.randint(0, list_len - 1)
+        print("\n====> 今日推荐游戏为：{} <====".format(list_copy.pop(random_number)))  # 这里我们在pop()的时候是对着列表的镜像进行pop()，故原列表不会受到任何影响
+        print("\n近期热点：")
+        for game in list_copy:  # 这里我们将迭代的列表变成列表的镜像
+            print(" * " + game)
+
+    recommendation(video_games)
+
+# 用 python -i 运行这个脚本，然后反复调用 recommendation(video_games)，看看问题是不是解决了？
--- 2-2_列表进阶/ex2.py
+++ 2-2_列表进阶/ex2.py
@@ -8,19 +8,13 @@ kanji = '玛德哈尔桑胡索毕牛顿艾因斯坦哥白尼华兹拉夏洛克'
 def random_name():
     kanji_list = list(kanji)
     name = []
-    for i in range(3):
+    for i in range(random.randint(2, 4)):
         letter = kanji_list.copy().pop(random.randint(0, len(kanji_list) - 1))
         name.append(letter)
     output = ''.join(name)
     return output
 
+def random_persona():
+    return persona.copy().pop(random.randint(0, len(persona) - 1))
 
-
-
-
-
-
-
-    
-
-print('你的随机名字是：{}'.format(random_name()))
+print('你的随机名字是：{}{}'.format(random_persona(),random_name()))
